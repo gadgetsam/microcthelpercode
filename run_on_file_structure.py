cd2c7f31aa1a42e36a2ccfe6c3b6fa4bb4e8580b
gadgetsam <gadgetsam@gmail.com>
2019-06-26 16:03:59 -0700

    added multithreading

--- run_on_file_structure.py
+++ run_on_file_structure.py
@@ -2,22 +2,50 @@
 import os
 from skimage.external import tifffile as tiff
 from crop import crop
-
-def run_on(function, folder_path, output_path):
+import multiprocessing
+import time
+def run_on(function, folder_path, output_path, num_threads = 16):
     folder_path_len = len(folder_path)
     directory_list = [x[0] for x in os.walk(folder_path)]
-
+    queue = multiprocessing.Queue()
+    queue2 = multiprocessing.Queue()
     for directory in directory_list:
         if 'rec' in directory:
-            os.rmdir(output_path + directory[folder_path_len:])
-            os.mkdir(output_path + directory[folder_path_len:])
 
-            file_list = [x[2] for x in os.walk(directory)]
-            for file in file_list[0]:
-                image = tiff.imread(directory+"/"+file)
-                out_image = function(image)
-                tiff.imsave(output_path+directory[folder_path_len:]+"/"+file, image)
-                print(file)
+            queue.put(directory)
+    process_list = []
+    for _ in range(num_threads):
+        temp = multiprocessing.Process(target=run_on_files, args=(function,folder_path_len, queue, queue2))
+        temp.start()
+        process_list.append(temp)
+    while not queue.empty():
+        print(queue.qsize())
+        time.sleep(1)
+    while queue2.qsize != num_threads:
+        time.sleep(1)
+    for process in process_list:
+        process.stop()
+
+
+
+def run_on_files(function, folder_path_len, queue, queue2):
+    while not queue.empty():
+        directory = queue.get()
+        try:
+            os.rmdir(output_path + directory[folder_path_len:])
+        except:
+            pass
+        os.mkdir(output_path + directory[folder_path_len:])
+        file_list = [x[2] for x in os.walk(directory)]
+        for file in file_list[0]:
+            image = tiff.imread(directory + "/" + file)
+            out_image = function(image)
+            tiff.imsave(output_path + directory[folder_path_len:] + "/" + file, image)
+            # print(file)
+    queue2.put(1)
+
+
+
 
 
 
