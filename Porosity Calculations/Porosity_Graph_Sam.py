b110af5d7e421a9e7396b5ef40555ce8947a9262
gadgetsam <gadgetsam@gmail.com>
2019-07-01 15:39:37 -0700

    Added files to run on many datasets

--- Porosity Calculations/Porosity_Graph_Sam.py
+++ Porosity Calculations/Porosity_Graph_Sam.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
-
+from concat_csv import concat_csv
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
 
 
 
@@ -10,11 +12,48 @@ import matplotlib
 
 def graph(path):
     print(path)
-    data = pd.read_csv(path)
+    data = concat_csv(path)
     print(data.shape)
-    print(data[0].max())
+    print(data[2].max())
+    dx, dy = 1, 5
 
+    # generate 2 2d grids for the x & y bounds
+    y, x = np.mgrid[slice(0, 1300 + dy, dy),  # slice
+                    slice(0, 600+ dx, dx)]  # time
+    print(y.shape)
+    z = np.zeros([260,1300])
+    for row in z.iter
+    print(z.shape)
 
+    # x and y are bounds, so z should be the value *inside* those bounds.
+    # Therefore, remove the last value from the z array.
+    z = z[:-1, :-1]
+    levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
+
+    # pick the desired colormap, sensible levels, and define a normalization
+    # instance which takes data values and translates those into levels.
+    cmap = plt.get_cmap('PiYG')
+    norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+
+    fig, (ax0, ax1) = plt.subplots(nrows=2)
+
+    im = ax0.pcolormesh(x, y, z, cmap=cmap, norm=norm)
+    fig.colorbar(im, ax=ax0)
+    ax0.set_title('pcolormesh with levels')
+
+    # contours are *point* based plots, so convert our bound into point
+    # centers
+    cf = ax1.contourf(x[:-1, :-1] + dx / 2.,
+                      y[:-1, :-1] + dy / 2., z, levels=levels,
+                      cmap=cmap)
+    fig.colorbar(cf, ax=ax1)
+    ax1.set_title('contourf with levels')
+
+    # adjust spacing between subplots so `ax1` title and `ax0` tick labels
+    # don't overlap
+    fig.tight_layout()
+
+    plt.show()
 
 
 if __name__ == "__main__":
--- Porosity Calculations/Run_on_many_datasets.py
+++ Porosity Calculations/Run_on_many_datasets.py
@@ -8,7 +8,9 @@ def input_function(image):  # this is used to specify all the parameters to crop
 
 
 
-folders=['/media/samschickler/1F6D-D692/FiberForm_19A_air_760torr_13_fast']
+folders=["/data/nasa-ablation/FiberForm_13A_air_200torr_05/FiberForm_13A_air_200torr_05",
+         "/data/nasa-ablation/FiberForm_13A_air_200torr_05/FiberForm_15A_air_200torr_07",
+         "/data/nasa-ablation/FiberForm_13A_air_200torr_05/FiberForm_18A_air_200torr_08"]
 for index, folder in enumerate(folders):
     os.mkdir("csvs"+str(index))
     print(index)
